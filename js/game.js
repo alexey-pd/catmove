672ddb2450f802f809df741a4633cf64681d2399
dojdev <mr.dojdev@gmail.com>
2016-08-14 15:25:00 +0300

    v2

--- js/game.js
+++ js/game.js
@@ -1,18 +1,18 @@
 var stage = new createjs.Stage('game-canvas');
 var queue = new createjs.LoadQueue(false);
 var hero;
-
+var bugs = [];
 var TILE_WIDTH = 100;
 var TILE_HEIGHT = 83;
 
 
 queue.addEventListener('complete', init);
 queue.loadManifest([
-    //{id: 'hero', src: 'img/char-cat-girl.png'},
-    {id: 'hero', src: 'img/cat.png'},
+    {id: 'hero', src: 'img/char-pink-girl.png'},
     {id: 'stone', src: 'img/stone-block.png'},
     {id: 'water', src: 'img/water-block.png'},
     {id: 'grass', src: 'img/grass-block.png'},
+    {id: 'bug', src: 'img/cat.png'},
 ]);
 
 
@@ -23,9 +23,9 @@ function init(){
 
     createMap();
     createHero();
+    createBugs();
     bindEvents();
-
-    createjs.Ticker.addEventListener('tick', stage);
+    createTicker();
 
     stage.update();
 }
@@ -34,6 +34,61 @@ function createHero(){
     hero = new createjs.Bitmap(queue.getResult('hero'));
     resetHero();
     stage.addChild(hero);
+}
+
+function createBugs(){
+    for (var i = 0; i < 5; i++){
+        createBug();
+    }
+}
+
+function createBug(){
+    var bug = new createjs.Bitmap(queue.getResult('bug'));
+    bugs.push(bug);
+    resetBug(bug);
+    stage.addChild(bug);
+}
+
+function resetBug(bug){
+    bug.y = Math.floor(Math.random() * 4) * TILE_HEIGHT;
+    bug.x = TILE_WIDTH - 100;
+    bug.speed = Math.random() + Math.random();
+}
+
+function createTicker(){
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener('tick', onTick);
+}
+
+function onTick(e){
+    bugs.forEach(function(bug){
+        moveBug(bug, e.delta);
+    });
+
+    checkHit();
+    stage.update();
+}
+
+function moveBug(bug,delta){
+    bug.x += bug.speed * delta / 5;
+
+    if (bug.x > 7 * TILE_WIDTH){
+        resetBug(bug);
+    }
+}
+
+function checkHit(){
+    bugs.forEach(function(bug){
+        if(checkCollision(bug)){
+            resetHero();
+        }
+    });
+}
+
+function checkCollision(bug){
+    return hero.y == bug.y &&
+           hero.x < bug.x + TILE_WIDTH * 0.75 &&
+           bug.x < hero.x + TILE_WIDTH * 0.75
 }
 
 function resetHero(){
